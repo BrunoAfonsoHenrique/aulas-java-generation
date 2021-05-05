35f03ada1a6648e89cd634a67bf469567168cc07
Bruno Afonso Henrique <bruno_afonso_henrique@outlook.com>
2021-05-05 13:47:17 -0300

    Adicionado o exercicio 04

--- src/Atividade02/Exercicio04.java
+++ src/Atividade02/Exercicio04.java
@@ -9,18 +9,18 @@ public class Exercicio04 {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		double num, raiz, potencia;
+		double num, resultado;
 		
 		System.out.print("Digite um numero: ");
 		num = leia.nextFloat();
 		
 		if(num % 2 == 0) {
-			raiz = Math.sqrt(num);
-			System.out.printf("\n" + num + " é numero par. Raiz = %2.2f", raiz);
+			resultado = Math.sqrt(num);
+			System.out.printf("\n" + num + " é numero par. Raiz = %2.2f", resultado);
 		}
 		else {
-			potencia = Math.pow(num, 2);
-			System.out.println("\n" + num + " é numero ímpar. Potência = " + potencia);
+			resultado = Math.pow(num, 2);
+			System.out.println("\n" + num + " é numero ímpar. Potência = " + resultado);
 		}
 	}
 
